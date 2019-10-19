b729568884bc95add7f363c3183aeb1acff6aa2e
Alex Puello <apuello88@yahoo.com>
2019-10-19 11:28:14 -0400

    added inquirer modules to test conditions of user choices.

--- javascript/app.js
+++ javascript/app.js
@@ -1,22 +1,86 @@
+
+//Variables
 var axios = require("axios");
+var userMovie = "remember the titans";
+var userBand = "";
+var inquirer = require("inquirer")
 
-axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(function(response){
-    var res = response.data;
+// Setting up a prompt for the user.
+inquirer.prompt([
+    {
+        type: "list",
+        message: "Which would you prefer to search?",
+        choices: ["Movies", "Bands", "Music"],
+        name: "search"
 
-    var title = res.Title;
-    var releaseDate = res.Released;
-    var RTrating = JSON.stringify(res.Ratings[1].Value);
-    var producer = res.Production;
-    var language = res.Language;
-    var plot = res.Plot;
-    var cast = res.Actors;
+    },
 
-console.log("Title: "+title);
-console.log("Release Date: "+releaseDate);
-console.log("Rotten Tomatoes Rating: "+RTrating);
-console.log("Producer: "+producer);
-console.log("Language: "+language);
-console.log("Plot: "+plot);
-console.log("Actors: "+cast);
+]).then(function(inquirerResponse){
+    console.log(inquirerResponse.search)
+    if (inquirerResponse.search === "Movies"){
+        inquirePrompt ()
+    }
+})
 
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Movie search function that takes in user input to work with OMDB API.
+function movieSearch (movieTitle){
+    axios.get("http://www.omdbapi.com/?t="+movieTitle+"&y=&plot=short&apikey=trilogy").then(function(response){
+        var res = response.data;
+    
+        var title = res.Title;
+        var releaseDate = res.Released;
+        var RTrating = JSON.stringify(res.Ratings[1].Value);
+        var producer = res.Production;
+        var language = res.Language;
+        var plot = res.Plot;
+        var cast = res.Actors;
+    
+    console.log("Title: "+title);
+    console.log("Release Date: "+releaseDate);
+    console.log("Rotten Tomatoes Rating: "+RTrating);
+    console.log("Producer: "+producer);
+    console.log("Language: "+language);
+    console.log("Plot: "+plot);
+    console.log("Actors: "+cast);
+    
+    });
+    
+    }
+
+
+    function inquirePrompt (){
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "What movie would you like to search?",
+                name: "movie"
+        
+            },
+        
+        ]).then(function(inquirerResponse){
+            var userSearch = inquirerResponse.movie;
+            
+            if(userSearch){
+                movieSearch (userSearch)
+            }
+            else{
+                movieSearch("Mr. Nobody");
+            }
+           
+        })
+    };
+
