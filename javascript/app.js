bfc75fbab2d276dc6217f5af37d53c67d11ca4ab
Alex Puello <apuello88@yahoo.com>
2019-10-19 10:32:52 -0400

    successfully hooked up the OMDB API and created a git ignore file to lower the amount of memory used when committing to my repo in GitHub.

--- javascript/app.js
+++ javascript/app.js
@@ -1,8 +1,22 @@
-var fs = require("fs");
+var axios = require("axios");
 
-fs.writeFile("README.md", "utf8", function (err){
-    if (err){
-        console.log(err);
-    }
-    
+axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(function(response){
+    var res = response.data;
+
+    var title = res.Title;
+    var releaseDate = res.Released;
+    var RTrating = JSON.stringify(res.Ratings[1].Value);
+    var producer = res.Production;
+    var language = res.Language;
+    var plot = res.Plot;
+    var cast = res.Actors;
+
+console.log("Title: "+title);
+console.log("Release Date: "+releaseDate);
+console.log("Rotten Tomatoes Rating: "+RTrating);
+console.log("Producer: "+producer);
+console.log("Language: "+language);
+console.log("Plot: "+plot);
+console.log("Actors: "+cast);
+
 });
