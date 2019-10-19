98a4ca489a9087b5f7faf0f1c864f9c845237119
Alex Puello <apuello88@yahoo.com>
2019-10-19 14:14:50 -0400

    added spotify api and installed the spotify npm modules, now I am testing the spotify API against user inputs and setting defaults for blank user input. Also, added a log.txt file to take in every exchange between the api and the user.

--- javascript/liri.js
+++ javascript/liri.js
@@ -1,43 +1,55 @@
 
 //Variables
 var axios = require("axios");
-var userMovie = "remember the titans";
-var userBand = "";
-var inquirer = require("inquirer")
-
-// Setting up a prompt for the user.
-inquirer.prompt([
-    {
-        type: "list",
-        message: "Which would you prefer to search?",
-        choices: ["Movies", "Bands", "Music"],
-        name: "search"
-
-    },
-
-]).then(function(inquirerResponse){
-    console.log(inquirerResponse.search)
-    if (inquirerResponse.search === "Movies"){
-        inquirePrompt ()
-    }
+var fs = require("fs");
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify({
+    id:"4f223dd648564d609a7c63ac1937d2e4",
+    secret:"3e2874ddeeb14c208c9042df22de349a"
 })
 
+spotify.search({type:"track", query:"I want it that way"},function(err,data){
+if (err){
+    console.log("Error occurred" + err)
+}
+console.log(data);
+});
+
+var userBand = "";
+var inquirer = require("inquirer")
+var filteredSearch = process.argv.splice(3).join("+");
+var searchCriteria = process.argv[2];
+
+
+
+// if (searchCriteria === "movie-this"){
+//     movieSearch (filteredSearch)
+// }else if (searchCriteria === "do-what-it-says"){}
+
+// if (searchCriteria === "do-what-it-says"){
+
+// }
 
 
 
 
-
-
-
-
-
-
+// This will take what is output to the terminal and saves to the log.txt file.
+function logdata (data){
+fs.appendFile("log.txt", "\n"+data, function(err){
+    
+    if(err){
+        console.log(err);
+    }
+})
+}
 
 
 
 
 // Movie search function that takes in user input to work with OMDB API.
 function movieSearch (movieTitle){
+    
+    if (movieTitle){
     axios.get("http://www.omdbapi.com/?t="+movieTitle+"&y=&plot=short&apikey=trilogy").then(function(response){
         var res = response.data;
     
@@ -57,8 +69,32 @@ function movieSearch (movieTitle){
     console.log("Plot: "+plot);
     console.log("Actors: "+cast);
     
-    });
     
+    });
+}else if(!movieTitle){
+    movieTitle = "Mr. Nobody"
+    axios.get("http://www.omdbapi.com/?t="+movieTitle+"&y=&plot=short&apikey=trilogy").then(function(response){
+        var res = response.data;
+    
+        var title = res.Title;
+        var releaseDate = res.Released;
+        var RTrating = JSON.stringify(res.Ratings[1].Value);
+        var producer = res.Production;
+        var language = res.Language;
+        var plot = res.Plot;
+        var cast = res.Actors;
+    
+    console.log("Title: "+title);
+    console.log("Release Date: "+releaseDate);
+    console.log("Rotten Tomatoes Rating: "+RTrating);
+    console.log("Producer: "+producer);
+    console.log("Language: "+language);
+    console.log("Plot: "+plot);
+    console.log("Actors: "+cast);
+    
+    
+    });
+}
     }
 
 
@@ -83,4 +119,26 @@ function movieSearch (movieTitle){
            
         })
     };
+
+
+function Inquirer(){
+    // Setting up a prompt for the user.
+    // inquirer.prompt([
+    //     {
+    //         type: "list",
+    //         message: "Which would you prefer to search?",
+    //         choices: ["Movies", "Bands", "Music"],
+    //         name: "search"
+    
+    //     },
+    
+    // ]).then(function(inquirerResponse){
+    //     console.log(inquirerResponse.search)
+    //     if (inquirerResponse.search === "Movies"){
+    //         inquirePrompt ()
+    //     }
+    // })
+    
+    
+    }
 
