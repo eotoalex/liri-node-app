ec10c73ff602b2efd1f48e14476e1c1d2c931b75
Alex Puello <apuello88@yahoo.com>
2019-10-20 17:57:03 -0400

    hid all my client ID and passwords with the git ignore file set up. Also set up a .env file.

--- javascript/liri.js
+++ javascript/liri.js
@@ -1,48 +1,54 @@
 
 //Variables
+require("dotenv").config();
+var keys = require("./keys.js");
 var axios = require("axios");
 var fs = require("fs");
+// require("openurl").open("http://" + spotifyURL)
 var Spotify = require("node-spotify-api");
-var spotify = new Spotify({
-    id:"4f223dd648564d609a7c63ac1937d2e4",
-    secret:"3e2874ddeeb14c208c9042df22de349a"
-})
+var spotify = new Spotify(keys.spotify)
 var userBand = "";
 var inquirer = require("inquirer")
 var filteredSearch = process.argv.splice(3).join("+");
 var searchCriteria = process.argv[2];
+var allNodeArgs = process.argv;
+
+// This function takes in all the arguments passed to node and appends in the log.txt.
+logAllCommands (allNodeArgs);
 
 
+// fs.writeFile("log.txt", "Command Log,", function(err){
+//     if(err){
+//         console.log(err)
+//     } 
+// })
 
-// This is where the spotify api is accessed.
-function spotifyAPI(){
-spotify.search({type:"track", query:"I want it that way"},function(err,data){
-if (err){
-    console.log("Error occurred" + err)
+
+if (searchCriteria === "movie-this"){
+    movieSearch (filteredSearch)
+}else if (searchCriteria === "do-what-it-says"){
+    var index = 0;
+    readFileByIndex(index);
 }
-console.log(data);
-});
-}
+   
 
-// if (searchCriteria === "movie-this"){
-//     movieSearch (filteredSearch)
-// }else if (searchCriteria === "do-what-it-says"){}
 
-// if (searchCriteria === "do-what-it-says"){
 
-// }
+
+
 
 
 
 
 // This will take what is output to the terminal and saves to the log.txt file.
-function logdata (data){
-fs.appendFile("log.txt", "\n"+data, function(err){
+function logAllCommands (data){
+// fs.appendFile("log.txt", "\n"+data, function(err){
     
-    if(err){
-        console.log(err);
-    }
-})
+//     if(err){
+//         console.log(err);
+//     }
+// })
+console.log(data)
 }
 
 
@@ -99,8 +105,32 @@ function movieSearch (movieTitle){
 }
     }
 
+        // This is where the spotify api is accessed.
+    function spotifyAPI(songName){
+                spotify
+                .request('https://api.spotify.com/v1/search?q='+songName+'&type=track,playlist')
+                .then(function(data){
+                    var spotifyURL = data.tracks.items[0].album.external_urls.spotify;
+                    require("openurl").open(spotifyURL)
+                    console.log(spotifyURL);
+                });
+                
+        }
 
-    function inquirePrompt (){
+        function readFileByIndex(i) {
+            fs.readFile("random.txt", "utf8", function (err, data){
+                if(err){
+                    console.log(err)
+                }
+                data = data.split(",")[i];
+                data = data.split(" ").join("+");
+                
+                spotifyAPI(data)
+            })
+            }
+
+
+        function inquirePrompt (){
         inquirer.prompt([
             {
                 type: "input",
@@ -120,7 +150,7 @@ function movieSearch (movieTitle){
             }
            
         })
-    };
+            };
 
 
 function Inquirer(){
